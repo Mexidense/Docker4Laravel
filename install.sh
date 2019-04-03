7525518e5a6b29853ff7f9b1cf61f5c921ec06e5
Salvador B <mexidense@gmail.com>
2019-04-04 00:27:10 +0200

    Fixing artisan migrate 💪

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo installing last steps...
-cp ./.env.example ./.envchmod +x
+cp ./.env.example ./.env
 docker-compose build
 docker-compose up -d
 docker exec docker4laravel_app_1 php artisan key:generate
 docker exec docker4laravel_app_1 php artisan config:cache
-docker exec docker4laravel_app_1 php artisan migrate
+echo Installation completed
